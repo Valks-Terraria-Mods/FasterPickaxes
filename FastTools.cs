ac470c0588e7566e30a6b5658dd0721031c2e3b3
valkyrienyanko <sebastianbelle074@gmail.com>
2018-11-18 13:40:09 -0600

    release

---
+++ FastTools.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace FasterPickaxes
+{
+    class FastTools : GlobalItem
+    {
+        public override float UseTimeMultiplier(Item item, Player player) {
+            if (item.pick > 0 || item.axe > 0 || item.hammer > 0) {
+                if (item.damage > 0) {
+                    return 1.25f;
+                }
+            }
+            return 1f;
+        }
+    }
+}
